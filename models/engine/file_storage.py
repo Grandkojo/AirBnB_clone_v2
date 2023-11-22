29bccf3fad93af341840e2d791d9ad192236a743
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-11-22 10:23:10 +0000

    airbnb v2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -19,7 +19,8 @@ class FileStorage:
         """Returns a dictionary of models currently in storage"""
         if cls is None:
             return self.__objects
-        return {key: value for key, value in self.__objects.items() if isinstance(value, cls)}
+        return {key: value for key, value in self.__objects.items() if
+                isinstance(value, cls)}
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -54,9 +55,10 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
     def delete(self, obj=None):
         """Delete obj from __objects"""
         if obj:
