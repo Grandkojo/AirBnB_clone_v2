d1fd60b70c6ffbe5f02c1b69663972669133c30b
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-12-28 14:54:26 -0800

    Flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,36 +1,32 @@
 #!/usr/bin/python3
-
-"""This script returns hello hbnb or HBNB depending on the route"""
+"""This is hello hbnb module for flask"""
 
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """The function from hello hbnb"""
+@app.route('/', strict_slashes=False)
+def hello():
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """The function for hbnb"""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    """Returns c with additional text"""
     return f"C {text.replace('_', ' ')}"
 
-@app.route("/python/<text>", strict_slashes=False)
-def hpython(text):
-    """Returns default text, if text is NULL"""
-    if text is Null:
-        return "Python is cool"
-    else:
-        return f"Python {text.replace('_', ' ')}"
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    return f"Python {text.replace('_', ' ')}"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
