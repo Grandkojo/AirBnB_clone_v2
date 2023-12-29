3b599ffe1682a1a0e29aa90f6400e4b2531e6205
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-12-29 15:02:47 -0800

    Flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,7 +35,7 @@ def number(n):
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
-def number_template(n=None):
+def number_template(n):
     return render_template('5-number.html', n=n)
 
 
